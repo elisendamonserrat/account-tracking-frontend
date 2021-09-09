ad818edd654af43a99f723a8ec3b1cad969c2fcb
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-09-09 18:15:46 +0200

    refactor(component): refactor App.js class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
@@ -9,24 +9,22 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import { withAuth } from './providers/AuthProvider';
 
-class App extends Component {
-	render() {
-		const { isLoading } = this.props;
-		if (isLoading) {
-			return <div>loading ... </div>;
-		}
-		return (
-			<div className="container">
-				<h1>Basic React Authentication</h1>
-				<Navbar />
-				<Switch>
-					<AnonRoute path="/signup" component={Signup} />
-					<AnonRoute path="/login" component={Login} />
-					<PrivateRoute path="/private" component={Private} />
-				</Switch>
-			</div>
-		);
+const App = (props) => {
+	const { isLoading } = props;
+	if (isLoading) {
+		return <div>loading ... </div>;
 	}
+	return (
+		<div className="container">
+			<h1>Basic React Authentication</h1>
+			<Navbar />
+			<Switch>
+				<AnonRoute path="/signup" component={Signup} />
+				<AnonRoute path="/login" component={Login} />
+				<PrivateRoute path="/private" component={Private} />
+			</Switch>
+		</div>
+	);
 }
 
 export default withAuth(App);
